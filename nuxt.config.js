64296d39f41389f3a7d1e4ce352ee036fca67443
Maxim Zotov <mmmnumber37@gmail.com>
2021-05-20 00:24:57 +0300

    asdf

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  target: 'static',
+
+  router: {
+    mode: 'hash',
+    base: '/calc-fractions/',
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
